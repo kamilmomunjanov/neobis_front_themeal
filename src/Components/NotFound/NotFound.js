3b0ac9ad5a427ba3add1850e32258692ac427413
Kamil <kamilmomunjanov@gmail.com>
2023-05-20 11:58:11 +0600

    not found

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import "./NotFound.css";
 
 const NotFound = () => {
     return (
-        <div>
-           По этому адресу ничего не найдено!!!
+        <div id="notfound">
+            <div className="notfound">
+                <div className="notfound-404">
+                    <h1>404</h1>
+                    <h2>Page not found</h2>
+                </div>
+                <a href="#">Report</a>
+                <a href="https://neobis-front-themeal-mu.vercel.app/" target="_blank">Homepage</a>
+
+            </div>
         </div>
     );
 };
